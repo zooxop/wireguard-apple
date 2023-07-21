f2fc3019ed8663caeed351b0c71860a4882234dd
CheolHyun Mun <zooxop@gmail.com>
2023-07-21 14:57:00 +0900

    modified: down minimum supported version

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "WireGuardKit",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15)
+        .macOS(.v11),
+        .iOS(.v14)
     ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
